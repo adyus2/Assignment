fd081f8ebc74786ae0cc28e861dcd56fbae80356
adyus2 <2974727278@qq.com>
2025-02-24 22:04:25 +0800

    Add files via upload

---
+++ assignment2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(248, 96);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(272, 28);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(909, 96);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(239, 28);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(617, 245);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(191, 47);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "计算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.Location = new System.Drawing.Point(635, 98);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(146, 26);
+            this.cmbOperator.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(520, 355);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(400, 40);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1562, 869);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox cmbOperator;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ assignment2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 窗体加载时初始化
+        }
+
+        private void InitializeControls()
+        {
+            cmbOperator.Items.Clear();
+            cmbOperator.Items.AddRange(new object[] { "+", "-", "×", "÷" });
+            cmbOperator.SelectedIndex = 0;
+            lblResult.Text = "等待计算...";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs()) return;
+
+            double num1 = double.Parse(txtNum1.Text);
+            double num2 = double.Parse(txtNum2.Text);
+            string op = cmbOperator.SelectedItem.ToString();
+
+            try
+            {
+                double result = Calculate(num1, num2, op);
+                lblResult.Text = $"{num1} {op} {num2} = {result:F2}";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "计算错误";
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidateInputs()
+        {
+            bool isValid = true;
+            if (!double.TryParse(txtNum1.Text, out _))
+            {
+                MessageBox.Show("第一个数字无效", "输入错误");
+                isValid = false;
+            }
+            if (!double.TryParse(txtNum2.Text, out _))
+            {
+                MessageBox.Show("第二个数字无效", "输入错误");
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        private double Calculate(double num1, double num2, string op)
+        {
+            switch (op)
+            {
+                case "+": return num1 + num2;
+                case "-": return num1 - num2;
+                case "×": return num1 * num2;
+                case "÷":
+                    if (Math.Abs(num2) < 1e-10)
+                        throw new DivideByZeroException("除数不能为零");
+                    return num1 / num2;
+                default: throw new ArgumentException("未知运算符");
+            }
+        }
+    }
+}
