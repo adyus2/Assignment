e8e97986311071dcbec37258adfa7ea3f053cb26
adyus2 <2974727278@qq.com>
2025-04-22 23:50:30 +0800

    Add files via upload

---
+++ assignment7(searchRes)/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace assignment7_searchRes_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBaiduResult = new System.Windows.Forms.TextBox();
+            this.txtBingResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(958, 166);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(126, 28);
+            this.txtKeyword.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(768, 262);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(97, 31);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(503, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "百度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1076, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "必应";
+            // 
+            // txtBaiduResult
+            // 
+            this.txtBaiduResult.Location = new System.Drawing.Point(487, 465);
+            this.txtBaiduResult.Multiline = true;
+            this.txtBaiduResult.Name = "txtBaiduResult";
+            this.txtBaiduResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBaiduResult.Size = new System.Drawing.Size(133, 17);
+            this.txtBaiduResult.TabIndex = 5;
+            // 
+            // txtBingResult
+            // 
+            this.txtBingResult.Location = new System.Drawing.Point(1024, 465);
+            this.txtBingResult.Multiline = true;
+            this.txtBingResult.Name = "txtBingResult";
+            this.txtBingResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBingResult.Size = new System.Drawing.Size(148, 21);
+            this.txtBingResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1844, 898);
+            this.Controls.Add(this.txtBingResult);
+            this.Controls.Add(this.txtBaiduResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "搜索内容";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBaiduResult;
+        private System.Windows.Forms.TextBox txtBingResult;
+    }
+}
+
---
+++ assignment7(searchRes)/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment7_searchRes_
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private async void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtKeyword.Text.Trim();
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("请输入搜索关键词");
+                return;
+            }
+
+            txtBaiduResult.Clear();
+            txtBingResult.Clear();
+
+            try
+            {
+                Task baiduTask = SearchAndExtract("baidu", keyword, txtBaiduResult);
+                Task bingTask = SearchAndExtract("bing", keyword, txtBingResult);
+
+                await Task.WhenAll(baiduTask, bingTask);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"发生错误: {ex.Message}");
+            }
+        }
+
+        private async Task SearchAndExtract(string engine, string keyword, TextBox resultBox)
+        {
+            try
+            {
+                string url;
+                switch (engine.ToLower())
+                {
+                    case "baidu":
+                        url = "https://www.baidu.com/s?wd=" + Uri.EscapeDataString(keyword);
+                        break;
+                    case "bing":
+                        url = "https://www.bing.com/search?q=" + Uri.EscapeDataString(keyword);
+                        break;
+                    default:
+                        throw new ArgumentException("不支持的搜索引擎");
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+                    string html = await httpClient.GetStringAsync(url);
+                    string textContent = ExtractTextContent(html);
+
+                    this.Invoke(new Action(() =>
+                    {
+                        resultBox.Text = textContent.Length > 200
+                            ? textContent.Substring(0, 200) + "..."
+                            : textContent;
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    resultBox.Text = $"获取{engine}结果失败: {ex.Message}";
+                }));
+            }
+        }
+
+        private string ExtractTextContent(string html)
+        {
+            // 简单去除HTML标签
+            string text = Regex.Replace(html, "<[^>]+>", " ");
+            // 合并多个空白字符
+            text = Regex.Replace(text, @"\s+", " ");
+            return text.Trim();
+        }
+
+
+    }
+}
