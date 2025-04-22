0e8e7d5ed359b1b2806107ddb7fce69a8a0a3679
adyus2 <2974727278@qq.com>
2025-04-22 23:51:46 +0800

    Add files via upload

---
+++ assignment4（file）/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace assignment4_FileManager_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile1 = new System.Windows.Forms.Button();
+            this.btnSelectFile2 = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.lblFile1 = new System.Windows.Forms.Label();
+            this.lblFile2 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile1
+            // 
+            this.btnSelectFile1.Location = new System.Drawing.Point(495, 82);
+            this.btnSelectFile1.Name = "btnSelectFile1";
+            this.btnSelectFile1.Size = new System.Drawing.Size(126, 60);
+            this.btnSelectFile1.TabIndex = 0;
+            this.btnSelectFile1.Text = "text1";
+            this.btnSelectFile1.UseVisualStyleBackColor = true;
+            this.btnSelectFile1.Click += new System.EventHandler(this.btnSelectFile1_Click_1);
+            // 
+            // btnSelectFile2
+            // 
+            this.btnSelectFile2.Location = new System.Drawing.Point(900, 86);
+            this.btnSelectFile2.Name = "btnSelectFile2";
+            this.btnSelectFile2.Size = new System.Drawing.Size(153, 55);
+            this.btnSelectFile2.TabIndex = 1;
+            this.btnSelectFile2.Text = "text2";
+            this.btnSelectFile2.UseVisualStyleBackColor = true;
+            this.btnSelectFile2.Click += new System.EventHandler(this.btnSelectFile2_Click_1);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(680, 235);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(176, 63);
+            this.btnMerge.TabIndex = 2;
+            this.btnMerge.Text = "merge";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click_1);
+            // 
+            // lblFile1
+            // 
+            this.lblFile1.AutoSize = true;
+            this.lblFile1.Location = new System.Drawing.Point(515, 383);
+            this.lblFile1.Name = "lblFile1";
+            this.lblFile1.Size = new System.Drawing.Size(62, 18);
+            this.lblFile1.TabIndex = 3;
+            this.lblFile1.Text = "label1";
+            // 
+            // lblFile2
+            // 
+            this.lblFile2.AutoSize = true;
+            this.lblFile2.Location = new System.Drawing.Point(898, 390);
+            this.lblFile2.Name = "lblFile2";
+            this.lblFile2.Size = new System.Drawing.Size(62, 18);
+            this.lblFile2.TabIndex = 4;
+            this.lblFile2.Text = "label2";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1519, 752);
+            this.Controls.Add(this.lblFile2);
+            this.Controls.Add(this.lblFile1);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnSelectFile2);
+            this.Controls.Add(this.btnSelectFile1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile1;
+        private System.Windows.Forms.Button btnSelectFile2;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label lblFile1;
+        private System.Windows.Forms.Label lblFile2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ assignment4（file）/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment4_FileManager_
+{
+    public partial class Form1 : Form
+    {
+        private string filePath1 = "";
+        private string filePath2 = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelectFile1_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath1 = openFileDialog.FileName;
+                lblFile1.Text = filePath1;
+            }
+        }
+
+        private void btnSelectFile2_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath2 = openFileDialog.FileName;
+                lblFile2.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnMerge_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath1) || string.IsNullOrEmpty(filePath2))
+            {
+                MessageBox.Show("请先选择两个文件！");
+                return;
+            }
+
+            try
+            {
+                // 读取文件内容
+                string content1 = File.ReadAllText(filePath1);
+                string content2 = File.ReadAllText(filePath2);
+
+                // 创建目标目录
+                string dataDir = Path.Combine(Application.StartupPath, "Data");
+                Directory.CreateDirectory(dataDir);
+
+                // 生成新文件名
+                string newFilePath = Path.Combine(dataDir, $"merged_{DateTime.Now:yyyyMMddHHmmss}.txt");
+
+                // 合并写入文件
+                File.WriteAllText(newFilePath, content1 + Environment.NewLine + content2);
+
+                MessageBox.Show($"文件合并成功！\n保存路径：{newFilePath}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"操作失败：{ex.Message}");
+            }
+        }
+    }
+}
